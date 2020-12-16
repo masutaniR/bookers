f23b4b409917c53cfd421406259d234e75734764
masutaniR <popohu87@gmail.com>
2020-12-16 08:28:41 +0000

    [add] 3-8 投稿一覧表示、編集・削除機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,33 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
     @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/books'
+    redirect_to book_path(book.id)
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book =Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
