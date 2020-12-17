85ac9197483d461f0170bc4ae121f8ee8bd95566
masutaniR <popohu87@gmail.com>
2020-12-17 08:17:49 +0000

    [Fix] 3-8 バリデーション正常化

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,10 +9,11 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    if book.save
-      redirect_to book_path(book.id), notice: 'Book was successfully created.'
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id), notice: 'Book was successfully created.'
     else
+      @books = Book.all
       render :index
     end
   end
@@ -22,9 +23,9 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
-      redirect_to book_path(book.id), notice: 'Book was successfully updated.'
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice: 'Book was successfully updated.'
     else
       render :edit
     end
